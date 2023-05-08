0a476ef35ec8f607c376adf372070d9a2cc23d72
JONATHAN FERNANDEZ <jonathan.fernandez-cepedal.external@capgemini.com>
2023-05-08 11:26:52 +0200

    puseo mi contenido

--- test/main.tf
+++ test/main.tf
@@ -1,5 +1,11 @@
+
+
+
+
+
+
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = "${random_pet.prefix.id}-rg"
+  name     = var.nombre_rg
+  ip = var.ip
 }
-
