caa9b1cc9e3809a1ae8a5c5fe74200a337bf12a1
saritamfd <sarita-milagros.flores-diaz.external@capgemini.com>
2023-05-09 08:43:33 +0200

    Subiendo toda la estructura con las variables

--- terraform/NETWORK/network.tf
+++ terraform/NETWORK/network.tf
@@ -11,7 +11,7 @@ resource "azurerm_network_interface" "grupo3_nic" {
 
   ip_configuration {
     name                          = "configuracion_del_nic"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    subnet_id                     = azurerm_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
   }
