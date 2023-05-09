fba055d53261798c7e98ba706ac1e4b24105bcce
diegodg1332 <diego.dominguez-gutierrez.external@capgemini.com>
2023-05-09 08:48:33 +0200

    commit prueba

--- net/variables.tf
+++ net/variables.tf
@@ -1,28 +1,24 @@
 # Definición de variables
 variable "location" {
 type = string
-default = "eastus"
 }
 
 
 
 variable "resource_group_name" {
 type = string
-default = "my-resource-group"
 }
 
 
 
 variable "address_space" {
 type = list(string)
-default = ["10.0.0.0/16"]
 }
 
 
 
 variable "subnet_name" {
 type = string
-default = "subnet-1"
 }
 
 
@@ -30,5 +26,4 @@ default = "subnet-1"
 
 variable "subnet_address_prefix" {
 type = string
-default = "10.0.1.0/24"
 }
